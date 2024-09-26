0d20c6a56a4d19d7c437f6e470fab2c57be352dd
ARPIT JAIN <131249093+ARPITJAINECE@users.noreply.github.com>
2024-09-26 18:06:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
-
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.114.0"
     }
+  }
+  
+  backend "azurerm" {
+    resource_group_name  = azurerm_resource_group.main.name
+    storage_account_name  = azurerm_storage_account.main.name
+    container_name        = azurerm_storage_container.main.name
+    key                   = "terraform.tfstate"
   }
 }
 
@@ -46,7 +53,7 @@ resource "azurerm_storage_account" "main" {
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type  = "GRS"
   
   tags = {
     keyForStorageAccount = "valueForStorageAccount"
