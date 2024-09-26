230f4a2047a3509426535790bfde76afb64bc0b3
ARPIT JAIN <131249093+ARPITJAINECE@users.noreply.github.com>
2024-09-26 17:20:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name  = var.storage_account_name
-    container_name        = var.container_name
-    key                   = "terraform.tfstate"  # State file name
-  }
 
   required_providers {
     azurerm = {
