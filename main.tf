91f986ed95b64e0da5dc2e1f961b29ec72ef7819
ARPIT JAIN <131249093+ARPITJAINECE@users.noreply.github.com>
2024-09-26 14:11:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
   subscription_id = "7ce53ffe-80a5-42a6-af05-890a8d744280"
-  client_id       = "f63b7b95-2b3b-4767-abf2-239d7a7ba4c3"
-  client_secret   = "mOg8Q~RDlQbXDsIOmh7gj2-pOfdMjYqTP_RNabvj"
+  client_id       = "7b5b0a33-29ab-489c-868b-8344bc5c99e2"
+  client_secret   = "zl48Q~lx53_YeVUp~18hnKYzf0QtM3gBwRFhPc7Z"
   tenant_id       = "a8fa88d0-1b32-43dc-a2ce-09a8f7ac140f"
   features {
 
